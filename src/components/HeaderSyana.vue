b9cf80ceab4ff20c8330ee09cf1bfaeb43e0a82f
Harles <haryobimo8@gmail.com>
2021-02-24 23:13:07 +0700

    updated version 1.1
    Sudah template sudah di implementasi dan sudah di connect ke api backend laravel

--- src/components/HeaderSyana.vue
+++ src/components/HeaderSyana.vue
@@ -19,9 +19,11 @@
                 <div class="row">
                     <div class="col-lg-2 col-md-2">
                         <div class="logo">
-                            <a href="./index.html">
+                            <router-link to="/">
+                            <a href="">
                                 <img src="img/logo_website_shayna.png" alt="" />
                             </a>
+                            </router-link>
                         </div>
                     </div>
                     <div class="col-lg-7 col-md-7"></div>
@@ -31,50 +33,44 @@
                                 Keranjang Belanja &nbsp;
                                 <a href="#">
                                     <i class="icon_bag_alt"></i>
-                                    <span>3</span>
+                                    <span>{{ keranjangUser.length }}</span>
                                 </a>
                                 <div class="cart-hover">
                                     <div class="select-items">
                                         <table>
-                                            <tbody>
-                                                <tr>
+                                            <tbody v-if="keranjangUser.length > 0">
+                                                 
+                                                <tr v-for="keranjang in keranjangUser" :key="keranjang.id">
                                                     <td class="si-pic">
-                                                        <img src="img/select-product-1.jpg" alt="" />
+                                                        <img class="photo-item" :src="keranjang.photo" alt="" />
                                                     </td>
                                                     <td class="si-text">
                                                         <div class="product-selected">
-                                                            <p>$60.00 x 1</p>
-                                                            <h6>Kabino Bedside Table</h6>
+                                                            <p>${{ keranjang.price }} x 1</p>
+                                                            <h6>{{ keranjang.name }}</h6>
                                                         </div>
                                                     </td>
-                                                    <td class="si-close">
+                                                    <td @click="removeItem(keranjang.id)" class="si-close">
                                                         <i class="ti-close"></i>
                                                     </td>
                                                 </tr>
-                                                <tr>
-                                                    <td class="si-pic">
-                                                        <img src="img/select-product-2.jpg" alt="" />
-                                                    </td>
-                                                    <td class="si-text">
-                                                        <div class="product-selected">
-                                                            <p>$60.00 x 1</p>
-                                                            <h6>Kabino Bedside Table</h6>
-                                                        </div>
-                                                    </td>
-                                                    <td class="si-close">
-                                                        <i class="ti-close"></i>
-                                                    </td>
-                                                </tr>
+                                              
                                             </tbody>
+                                            <tbody v-else>
+                                                <tr>
+                                                    <td>Keranjang Kosong</td>
+                                                </tr>
+                                             </tbody>
                                         </table>
                                     </div>
                                     <div class="select-total">
                                         <span>total:</span>
-                                        <h5>$120.00</h5>
+                                        <h5>${{ totalHarga }}.00</h5>
                                     </div>
                                     <div class="select-button">
-                                        <a href="#" class="primary-btn view-card">VIEW CARD</a>
-                                        <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
+                                        <router-link to="/cart" class="primary-btn view-card">VIEW CARD
+                                        </router-link>
+                                        <router-link to="#" class="primary-btn checkout-btn">CHECK OUT</router-link>
                                     </div>
                                 </div>
                             </li>
@@ -91,6 +87,45 @@
 <script>
 export default {
     name : 'HeaderSyana',
-    
+    data () {
+      return{
+          keranjangUser: []
+      }
+  },
+  methods: {
+      removeItem(idx){
+        let keranjangUserStorage = JSON.parse(localStorage.getItem('keranjangUser'))
+        let itemKeranjangUserStorage = keranjangUserStorage.map(itemKeranjangUserStorage => itemKeranjangUserStorage.id)
+        let index = itemKeranjangUserStorage.findIndex(id => id == idx)
+        this.keranjangUser.splice(index,1)
+        const parsed = JSON.stringify(this.keranjangUser)
+         localStorage.setItem('keranjangUser',parsed)
+
+         window.location.reload()
+      }
+  },
+    mounted () {
+         if(localStorage.getItem('keranjangUser')){
+            try{
+                this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'))
+            }catch(e){
+                localStorage.removeItem('keranjangUser');
+            }
+        }
+    },
+    computed: {
+        totalHarga(){
+            return this.keranjangUser.reduce(function(items, data){
+                return items + data.price;
+            },0)
+        }
+    },
 }
 </script>
+
+<style scoped>
+.photo-item{
+    width: 80px;
+    height: 80px;
+}
+</style>
